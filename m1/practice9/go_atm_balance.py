69ab05de70e44cc1c4ac25d55ec736715c7cc1e9
GoChanelle <gochanellemei@gmail.com>
2026-09-08 16:51:55 +0800

    Update go_atm_balance.py

---
+++ m1/practice9/go_atm_balance.py
@@ -0,0 +1,20 @@
+# TODO 1: Create a function named check_balance().
+def check_balance(account):
+    
+    # TODO 2: Ask the Account object for its current balance.
+    return account.check_balance()
+
+""" 
+######### Learning Signature ######### 
+Programmed by: Chanelle Go
+Date Submitted: September 2, 2026
+ 
+Program Description: This program creates a balance module for the main code to call later.
+Reflection: I learned that a function can check the balance of an account by calling a method from the Account object.
+ 
+AI Usage
+[/] No AI Assistance – Completed independently without AI.
+[ ] AI as Support Tool – Used AI for explanations, syntax, or minor corrections.
+[ ] AI as Collaborative Partner – Used AI to design, structure, or co-create significant code.
+"""   
+ 

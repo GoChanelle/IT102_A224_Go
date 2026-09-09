fadf1a9792d88c1f841147d1e8e3d95364fa87bc
GoChanelle <gochanellemei@gmail.com>
2026-09-09 20:01:26 +0800

    v

--- m1/practice9/go_atm_main.py
+++ m1/practice9/go_atm_main.py
@@ -9,7 +9,7 @@ import go_atm_analysis
  
  
 # TODO 7: Create the Account object.
-account = Account("Chanelle Go", 10000.00)
+account = Account("Chanelle Go", 1000.00)
   
 # TODO 8: Configure the Streamlit page.
 st.set_page_config(

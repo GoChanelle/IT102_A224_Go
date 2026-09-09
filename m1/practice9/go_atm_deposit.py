cdc3d519af184ff165ee4981892f628aa1352a77
GoChanelle <gochanellemei@gmail.com>
2026-09-09 14:49:56 +0800

    move check balance

--- m1/practice9/go_atm_deposit.py
+++ m1/practice9/go_atm_deposit.py
@@ -35,21 +35,7 @@ def deposit_money(account, amount):
 # TODO 12: Return False when the transaction is unsuccessful.
     return False
  
- # CHECK BALANCE HERE
-# TODO 14: Check whether the selected option "Check Balance".
-if choice == "Check Balance":
  
-# TODO 15: Display a page header.
-  st.header("Check Balance");
- 
-# TODO 16: Call the balance module and obtain the current account balance.
-  balance = (go_atm_balance.check_balance(account))
- 
-# TODO 17: Display the balance using a Streamlit metric.
-  st.metric(
-    "Current Balance",
-    f"P{balance:,.2f}"
-    )
 
 """ 
 ######### Learning Signature ######### 
--- m1/practice9/go_atm_main.py
+++ m1/practice9/go_atm_main.py
@@ -54,6 +54,21 @@ choice = st.sidebar.radio(
   ]
 )
 
+# CHECK BALANCE HERE
+# TODO 14: Check whether the selected option "Check Balance".
+if choice == "Check Balance":
+ 
+# TODO 15: Display a page header.
+  st.header("Check Balance");
+ 
+# TODO 16: Call the balance module and obtain the current account balance.
+  balance = (go_atm_balance.check_balance(account))
+ 
+# TODO 17: Display the balance using a Streamlit metric.
+  st.metric(
+    "Current Balance",
+    f"P{balance:,.2f}"
+    )
 
 ######### Learning Signature ######### 
 #Programmed by: Chanelle Go

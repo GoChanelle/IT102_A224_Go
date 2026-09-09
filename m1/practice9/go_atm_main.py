19a16a0efca2beb047114cf4e545a2f1b04a34e4
GoChanelle <gochanellemei@gmail.com>
2026-09-09 14:40:20 +0800

    fix indentation errors

--- m1/practice9/go_atm_main.py
+++ m1/practice9/go_atm_main.py
@@ -53,7 +53,7 @@ choice = st.sidebar.radio(
     "Analyze Transactions",
   ]
 )
-
+# CHECK BALANCE HERE
 # TODO 14: Check whether the selected option "Check Balance".
 if choice == "Check Balance":
  
@@ -69,7 +69,7 @@ if choice == "Check Balance":
     f"P{balance:,.2f}"
     )
 
-
+# DEPOSIT HERE
 # TODO 18: Add the "Deposit" branch.
 elif choice == "Deposit":
  
@@ -77,33 +77,33 @@ elif choice == "Deposit":
   st.header("Deposit Money")
  
 # TODO 20: Create a number input.
-amount = st.number_input(
-  "Enter deposit amount",
-  min_value=0.0,
-  step=100.0,
-  format="%.2f"
-) 
+  amount = st.number_input(
+    "Enter deposit amount",
+    min_value=0.0,
+    step=100.0,
+    format="%.2f"
+  ) 
  
 # TODO 21: Create a button named: Deposit Money
-if st.button("Deposit Money"):
+  if st.button("Deposit Money"):
   
 # TODO 22: When the button is clicked, check whether the amount is valid.
 # TODO 23: If the amount is invalid, display a Streamlit error message. 
-  if amount <= 0:
-    st.error("Invalid deposit amount.")
+    if amount <= 0:
+      st.error("Invalid deposit amount.")
 
 # TODO 24: Otherwise, call the deposit module.  
-  else:
-    success = (
-      go_atm_deposit.deposit_money(account, amount)
-    )
+    else:
+      success = (
+        go_atm_deposit.deposit_money(account, amount)
+      )
 
 # TODO 25: If the deposit is successful, display a success message.
-  if success:
-    st.success("Deposit Successful.")
+    if success:
+      st.success("Deposit Successful.")
  
 # TODO 26: Display the updated balance using a Streamlit metric.
-  st.metric(
-    "New Balance",
-    f"P{account.check_balance():,.2f}"
-  )
+    st.metric(
+      "New Balance",
+      f"P{account.check_balance():,.2f}"
+    ) 
